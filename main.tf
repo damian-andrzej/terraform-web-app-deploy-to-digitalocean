1c5bf21f57a8051d804a3261732a0bd88c8d35da
damian-andrzej <102800704+damian-andrzej@users.noreply.github.com>
2025-03-10 20:21:52 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,15 +4,15 @@ terraform {
     key= "terraform.tfstate"        # Path where Terraform state will be stored
     region="fra1"                     # DigitalOcean region
     endpoint= "https://s3-backend.fra1.digitaloceanspaces.com"  # Endpoint URL for DigitalOcean Spaces
-    access_key= var.s3_access_key_id          # Access key from your GitHub Secrets
-    secret_key= var.s3_secret_key_secret           # Secret key from your GitHub Secrets
+    access_key= var.s3_access_key          # Access key from your GitHub Secrets
+    secret_key= var.s3_secret_key          # Secret key from your GitHub Secrets
     skip_credentials_validation= true
     skip_metadata_api_check= true
   }
 }
 
 provider "digitalocean" {
-  token = var.token
+  token = var.TF_VAR_do_token
 }
 
 resource "digitalocean_droplet" "example" {
@@ -20,7 +20,7 @@ resource "digitalocean_droplet" "example" {
   region= "fra1"
   size= "s-1vcpu-512mb-10gb"
   image= "ubuntu-22-04-x64"
-  ssh_keys= [var.ssh]
+  ssh_keys= [var.TF_VAR_ssh_fingerprint]
   backups = false
   monitoring = false
 }
