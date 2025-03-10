019f6aa3969951c571de8a01c9a360de4c886fc2
damian-andrzej <102800704+damian-andrzej@users.noreply.github.com>
2025-03-10 20:15:38 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 terraform {
   backend "s3" {
-    bucket                      = "s3-backend"               # Name of your DigitalOcean Space (S3 bucket)
-    key                         = "terraform.tfstate"        # Path where Terraform state will be stored
-    region                      = "fra1"                     # DigitalOcean region
-    endpoint                    = "https://s3-backend.fra1.digitaloceanspaces.com"  # Endpoint URL for DigitalOcean Spaces
-    access_key                  = var.s3_access_key_id          # Access key from your GitHub Secrets
-    secret_key                  = var.s3_secret_key_secret           # Secret key from your GitHub Secrets
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
+    bucket = "s3-backend"               # Name of your DigitalOcean Space (S3 bucket)
+    key= "terraform.tfstate"        # Path where Terraform state will be stored
+    region="fra1"                     # DigitalOcean region
+    endpoint= "https://s3-backend.fra1.digitaloceanspaces.com"  # Endpoint URL for DigitalOcean Spaces
+    access_key= var.s3_access_key_id          # Access key from your GitHub Secrets
+    secret_key= var.s3_secret_key_secret           # Secret key from your GitHub Secrets
+    skip_credentials_validation= true
+    skip_metadata_api_check= true
   }
 }
 
@@ -16,19 +16,19 @@ provider "digitalocean" {
 }
 
 resource "digitalocean_droplet" "example" {
-  name     = "github-actions-droplet"
-  region   = "fra1"
-  size     = "s-1vcpu-512mb-10gb"
-  image    = "ubuntu-22-04-x64"
-  ssh_keys = [var.ssh]
+  name= "github-actions-droplet"
+  region= "fra1"
+  size= "s-1vcpu-512mb-10gb"
+  image= "ubuntu-22-04-x64"
+  ssh_keys= [var.ssh]
   backups = false
   monitoring = false
 }
 
 variable "do_token" {
   description = "DigitalOcean API token"
-  type        = string
-  sensitive   = true
+  type= string
+  sensitive = true
 }
 
 variable "ssh" {
